7f06309026f51d41e01024c697898c844f8fde73
Official-Nethmina <nbhashitha159@gmail.com>
2025-08-06 15:42:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+  SESSION_ID: process.env.SESSION_ID || "Yf1XzAAa#duBbCz5aIoxo7W0os1WdYitucc0UUpXmhZT7Wyi7Sd8",
   OWNER_NUM: process.env.OWNER_NUM || "94789086040",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/VYHLZ8Cv/my-data.jpg",
